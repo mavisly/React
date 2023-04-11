7ca47de35cb45668a02e09943637ed6ee31446d0
Mavisly <mavi.04.02.03@gmail.com>
2023-04-11 13:48:19 -0300

    Criação do primeiro componente

---
+++ src/Paginas/Home/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Home.css";
+
+function Home(){
+    return(
+     <>
+        <h1  className="titulo">Seja bem-vindo ao mundo bruxo!</h1>
+        <img src="src\assets\imagens\varinha-hp.png" 
+        alt="Varinha mágica"
+        className="img" />
+     </>
+    );
+}
+
+export default Home;
