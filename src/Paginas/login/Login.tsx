f8900949db983ce0e1491d58817f14b47e6b569e
Mavisly <mavi.04.02.03@gmail.com>
2023-04-17 15:02:17 -0300

    Implementação de segurança

--- src/Paginas/login/Login.tsx
+++ src/Paginas/login/Login.tsx
@@ -1,40 +1,80 @@
-import "./Login.css";
-import React from "react";
-import { Grid, Typography, Button, TextField } from "@material-ui/core";
-import { Box } from "@mui/material";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect, ChangeEvent } from 'react';
+import { Grid, Typography, TextField, Button } from '@material-ui/core';
+import {Box} from '@mui/material';
+import { Link, useNavigate } from 'react-router-dom';
+import useLocalStorage from 'react-use-localstorage';
+import { login } from '../../services/Service';
+import UsuarioLogin from '../../models/UsuarioLogin';
+import './Login.css';
 
 function Login() {
-    return (
-        <>
-            <Grid container direction='row' justifyContent='center' alignItems='center'>
-                <Grid alignItems='center' xs={6}>
-                    <Box paddingX={20}>
-                        <form>
-                            <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>Entrar</Typography>
-                            <TextField id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
-                            <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
-                            <Box marginTop={2} textAlign='center'>
-                                <Link to='/home' className='text-decorator-none'>
-                                    <Button type='submit' variant='contained' color='primary'>
-                                        Logar
-                                    </Button>
-                                </Link>
-                            </Box>
-                        </form>
-                        <Box display='flex' justifyContent='center' marginTop={2}>
-                            <Box marginRight={1}>
-                                <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
-                            </Box>
-                            <Typography variant='subtitle1' gutterBottom align='center' className='textos1'>Cadastre-se</Typography>
-                        </Box>
-                    </Box>
-                </Grid>
-                <Grid xs={6} className='imagem'>
+    let navigate = useNavigate();
+    const [token, setToken] = useLocalStorage('token');
+    const [usuarioLogin, setUsuarioLogin] = useState<UsuarioLogin>(
+        {
+            id: 0,
+            nome:'',
+            usuario: '',
+            senha: '',
+            token: '',
+            foto:''
+        }
+        )
 
-                </Grid>
+        function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+
+            setUsuarioLogin({
+                ...usuarioLogin,
+                [e.target.name]: e.target.value
+            })
+        }
+
+            useEffect(()=>{
+                if(token != ''){
+                    navigate('/home')
+                }
+            }, [token])
+
+        async function onSubmit(e: ChangeEvent<HTMLFormElement>){
+            e.preventDefault();
+            try{
+                 await login(`/usuarios/logar`, usuarioLogin, setToken);
+
+                alert('Usuário logado com sucesso!');
+            }catch(error){
+                alert('Dados do usuário inconsistentes. Erro ao logar!');
+            }
+        }
+
+    return (
+        <Grid container direction='row' justifyContent='center' alignItems='center'>
+            <Grid alignItems='center' xs={6}>
+                <Box paddingX={20}>
+                    <form onSubmit={onSubmit}>
+                        <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>Entrar</Typography>
+                        <TextField value={usuarioLogin.usuario} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
+                        <TextField value={usuarioLogin.senha} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password'fullWidth />
+                        <Box marginTop={2} textAlign='center'>
+                                <Button type='submit' variant='contained' color='primary'>
+                                    Logar
+                                </Button>
+                        </Box>
+                    </form>
+                    <Box display='flex' justifyContent='center' marginTop={2}>
+                        <Box marginRight={1}>
+                            <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
+                        </Box>
+                        <Link to='/cadastro'>
+                            <Typography variant='subtitle1' gutterBottom align='center' className='textos1'>Cadastre-se</Typography>
+                        </Link>
+                            
+                    </Box>
+                </Box>
             </Grid>
-        </>
+            <Grid xs={6} className='imagem'>
+
+            </Grid>
+        </Grid>
     );
 }
 
